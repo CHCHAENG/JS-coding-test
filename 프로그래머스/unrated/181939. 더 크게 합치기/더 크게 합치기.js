f0f09379f40a488b4313119fe204b8540a1a5bc8
CHCHAENG <oco6029@naver.com>
2023-07-22 17:23:20 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    let answer = 0;
+    const ab = (String(a)+String(b));
+    const ba = (String(b)+String(a));
+               
+    answer = (ab >= ba) ? ab : ba
+    return Number(answer);
+}
