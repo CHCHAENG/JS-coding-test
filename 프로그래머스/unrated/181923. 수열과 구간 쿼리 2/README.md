3780982767d5edf2668b3e0c923c9f40722eb73a
CHCHAENG <oco6029@naver.com>
2023-07-24 23:16:49 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.40 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [unrated] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 3.51 ms
+메모리: 36.8 MB, 시간: 3.40 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -3,20 +3,19 @@ function solution(arr, queries) {
     
     queries.forEach(([a, b, c], index) => {
         k_i = -1;
-    // console.log('row', a, b, c);
+        
         for (let i = a; i <= b; i++) {
-             
-                if (arr[i] > c) {
-                    
-                    if (k_i > arr[i]) k_i = arr[i];
-                    else if (k_i !== -1) k_i = k_i;
-                    else k_i = arr[i];
-                } 
-            
+            if (arr[i] > c) {
+                if (k_i !== -1) {
+                    if (k_i > arr[i])
+                        k_i = arr[i];
+                } else {
+                    k_i = arr[i];
+                }   
+            }  
         }
         
         answer.push(k_i);
-        // console.log('k : ', k_i);
     })
     
     return answer;
