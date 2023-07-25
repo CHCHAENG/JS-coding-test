0310e2143bdc096489db4fbf0f279cc4ec29b1ab
CHCHAENG <oco6029@naver.com>
2023-07-26 00:43:04 +0900

    [unrated] Title: 배열 만들기 2, Time: 5.11 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,25 +1,4 @@
 function solution(l, r) {
     var answer = [];
-    
-    for (let i = l; i <= r; i++) {
-        if (i % 5 === 0){
-            str = i.toString();
-            check = true;
-            
-            for (let i = 1; i <= 9; i++) {
-                if (i === 5)
-                    continue;
-                if (str.includes(i.toString()))
-                    check = false;
-                    
-            }
-            if (check) 
-                answer.push(i);
-        }
-    }
-    
-    if (answer.length === 0)
-        answer.push(-1);
-    
     return answer;
 }
