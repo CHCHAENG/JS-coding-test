ff80ca7f3fdc6957e627f43901a3e66cb8f1047c
CHCHAENG <oco6029@naver.com>
2023-07-26 00:34:01 +0900

    [unrated] Title: 배열 만들기 4, Time: 5.54 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    var stk = [];
+    let i = 0;
+    
+    while (i < arr.length) {
+        
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+        } else {
+            if (stk.at(-1) < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            }
+            else {
+                stk = stk.slice(0, -1);
+            }
+        }
+    }
+    return stk;
+}
