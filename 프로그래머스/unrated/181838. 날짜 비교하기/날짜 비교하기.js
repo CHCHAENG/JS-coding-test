10b149b045a44bb5704db87ced875fc628aa620d
CHCHAENG <oco6029@naver.com>
2023-09-08 16:47:15 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,10 +1,3 @@
 function solution(date1, date2) {
-    if (date1[0] > date2[0]) return 0;
-    else if (date1[0] === date2[0]){
-        if (date1[1] === date2[1])
-            return date1[2] < date2[2] ? 1 : 0;
-        else 
-            return date1[1] < date2[1] ? 1 : 0;
-    }
-    else return 1;
+   return new Date(date1) < new Date(date2) ? 1 : 0
 }
