b79c07cda2d80c569616446949e1fcbff0d9f320
CHCHAENG <oco6029@naver.com>
2023-09-23 18:18:48 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,17 +1,11 @@
 function solution(brown, yellow) {
-    let answer = [];
-    
     let num = (brown + 4) / 2;
     
-    for (let i = Math.floor(num / 2); i < num - 2; i++) {
+    for (let i = num - 2; i >= Math.floor(num / 2); i--) {
         let a = i;
         let b = num - a;
         
-        if (a < b)
-            continue;
-        
-
-        if ((b - 2) * (a - 2) === yellow)
+        if ((a - 2) * (b - 2) === yellow)
             return [a, b];
     }
 }
