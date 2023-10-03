ba7615d17ce97b17e7460ad40b4afb6f5400943a
CHCHAENG <oco6029@naver.com>
2023-10-03 17:17:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,7 +2,7 @@ function solution(num_list) {
     let answer = [0, 0];
     
     num_list.map((v) => {
-        v % 2 === 0 ? answer[0]++ : answer[1]++;
+        answer[v % 2]++;
     })
     
     return answer;
