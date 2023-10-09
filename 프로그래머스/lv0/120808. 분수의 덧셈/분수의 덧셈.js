e841ee934b21b209ff650712c3cb65ac12e1ce79
CHCHAENG <oco6029@naver.com>
2023-10-09 16:13:01 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,21 +1,16 @@
+function findGCD(a, b) {
+    return a % b ? findGCD(b, (a % b)) : b;    
+}
+
 function solution(numer1, denom1, numer2, denom2) {
     let answer = [];
     
-    if (denom1 !== denom2) {
-         let num = denom1 * denom2;
-        answer[1] = num;
-        answer[0] = (numer1 * parseInt(num/denom1)) + (numer2 * parseInt(num/denom2));
-    } else {
-        answer[1] = denom1;
-        answer[0] = numer1 + numer2;
-    }
+    answer[0] = denom1 * numer2 + denom2 * numer1;
+    answer[1] = denom1 * denom2;
     
-    for (let i = 2; i <= answer[1]; i++) {
-        while (answer[0] % i === 0 && answer[1] % i === 0) {
-            answer[0] /= i;
-            answer[1] /= i;
-        }
-    }
-
+    let gcd = findGCD(answer[0], answer[1]);
+    answer[0] /= gcd;
+    answer[1] /= gcd;
+    
     return answer;
 }
