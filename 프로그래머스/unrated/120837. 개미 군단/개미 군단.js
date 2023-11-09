6ede1a11debcd1a8fe7fce383b793060722b2493
CHCHAENG <oco6029@naver.com>
2023-11-09 23:16:40 +0900

    [level unrated] Title: 개미 군단, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,23 +1,3 @@
 function solution(hp) {
-    let answer = 0;
-    
-    while (hp > 0) {
-        if (hp >= 5) {
-            let num = parseInt(hp / 5);
-            
-            answer += num;
-            
-            hp = hp - (5 * num);
-        }
-        else if (hp >= 3) {
-            let num = parseInt(hp / 3);
-            answer += num;
-            
-            hp = hp - (3 * num);
-        } else {
-            answer += hp;
-            hp = 0;
-        }
-    }
-    return answer;
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5) % 3);
 }
