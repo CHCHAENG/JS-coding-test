07e8646e47b0c3b5baed338f82300f1d7ae1827d
CHCHAENG <oco6029@naver.com>
2023-11-13 21:52:46 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,32 +1,23 @@
 function solution(progresses, speeds) {
     let answer = [];
-    let cnt = [];
     
-    progresses.forEach((v, i) => {
-        let remain = 100 - v;
+    while (progresses.length > 0) {
         
-        if (remain % speeds[i] === 0) {
-            cnt.push(remain / speeds[i]);
-        } else {
-            cnt.push(parseInt(remain / speeds[i]) + 1);
+        for (let i in speeds) {
+            if (progresses[i] < 100)
+                progresses[i] += speeds[i];
         }
-    });
-    
-    
-    cnt.forEach((v, i) =>{
-        let c = 1;
         
-        if (v >= cnt[i + 1]) {   
-            while(v >= cnt[i + 1]) {
-                cnt.splice(i + 1, 1);
-                c++;
-            }
-            answer.push(c);
-            
-        } else {
-            answer.push(c);
+        
+        let cnt = 0;
+        while (progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            cnt++;
         }
-    });
+        
+        if (cnt > 0) answer.push(cnt);
+    }
     
     return answer;
 }
