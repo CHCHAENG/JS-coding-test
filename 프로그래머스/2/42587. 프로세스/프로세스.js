c61afc1250959af46e88f73d3fd4d83c931c025c
CHCHAENG <oco6029@naver.com>
2023-11-20 16:30:58 +0900

    [level 2] Title: 프로세스, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -3,23 +3,31 @@ function solution(priorities, location) {
     let answer = [];
 
     while (arr.length > 0) {
-        let val = arr.shift();
-        let flag = 0;
+        let val = arr.shift(); 
+        let pVal = priorities.shift();
         
-        for (let i = 1; i < arr.length; i++) {
-            if (priorities[0] < priorities[i]) {
-                flag = 1;
-                break;
-            }
-        }
-        
-        if (flag === 1) {
+        if (priorities.some((i) => pVal < i)) {
             arr.push(val);
-            priorities.push(priorities.shift());
-        } else  {
+            priorities.push(pVal);
+        } else {
             answer.push(val - 1);
-            priorities.shift();
         }
+//         let flag = 0;
+    
+//         for (let i = 1; i < arr.length; i++) {
+//             if (priorities[0] < priorities[i]) {
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+        
+//         if (flag === 1) {
+//             arr.push(val);
+//             priorities.push(priorities.shift());
+//         } else  {
+//             answer.push(val - 1);
+//             priorities.shift();
+//         }
     }
     
     return answer.indexOf(location) + 1;
