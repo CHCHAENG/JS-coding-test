ba0d7280185695467daa95be176e9381cb689b89
CHCHAENG <oco6029@naver.com>
2023-11-21 15:21:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,39 @@
+function solution(s) {
+    let numberList = {
+        "zero" : 0,
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9
+    };
+    
+    let checkStr = ["0", "1", "2", "3", "4", "5", "6", "7", "8" ,"9"];
+    
+    let answer = "";
+    let temp = "";
+    
+    [...s].forEach((v, i) => {
+        if (checkStr.includes(v)) {
+            if (temp !== "") {
+                answer += numberList[temp];
+            }
+               
+            answer += v;
+            temp = "";
+       
+        } else {
+            temp += v;
+            if (temp in numberList) {
+                answer += numberList[temp];
+                temp = "";
+            }
+        }
+    })
+    
+    return +answer;
+}
