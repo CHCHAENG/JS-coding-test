f595d8816d92296911cc81c1c370bb3a44be5a31
CHCHAENG <oco6029@naver.com>
2023-11-27 22:16:40 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,35 +1,36 @@
 function solution(tickets) {
-  let answer = [];
-  const result = [];
-  const visited = [];
-  
-  tickets.sort();
-  
-  const len = tickets.length;
-  const dfs = (str, count) => {
-    result.push(str);
+    tickets.sort();
     
-    if(count === len) {
-      answer = result;
-      return true;
+    let answer = [];
+    let result = [];
+    let visited = [];
+    
+    const length = tickets.length;
+    
+    const dfs = (start, count) => {
+        result.push(start);
+        
+        if (count === length) {
+            answer = result;
+            return true;
+        }
+        
+        for (let i = 0; i < length; i++) {
+            if (!visited[i] && tickets[i][0] === start) {
+                visited[i] = true;
+                
+                if (dfs(tickets[i][1], count + 1)) return true;
+                
+                visited[i] = false;
+            }
+        }
+        
+        result.pop();
+        
+        return false;
     }
     
-    for(let i = 0; i < len; i++) {
-      if(!visited[i] && tickets[i][0] === str) {
-        visited[i] = true;
-        
-        if(dfs(tickets[i][1], count+1)) return true;
-        
-        visited[i] = false;
-      }
-    }
-    
-    result.pop();
-    
-    return false;
-  }
-  
-  dfs("ICN", 0);
-  
-  return answer;
+    dfs("ICN", 0);
+    return answer;
 }
+  
