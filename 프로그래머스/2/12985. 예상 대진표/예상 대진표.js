8cb65ac5f8b5e695cf886f87cfcc5dd2ca21b602
CHCHAENG <oco6029@naver.com>
2023-12-06 16:58:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,7 +1,7 @@
 function solution(n,a,b) {
     let answer = 0;
     
-    while (parseInt(a - 1 / 2) !== parseInt(b - 1 / 2))  {
+    while (a !== b)  {
         a % 2 === 0 ? a = a / 2 : a = parseInt(a / 2) + 1;
         b % 2 === 0 ? b = b / 2 : b = parseInt(b / 2) + 1;
     
