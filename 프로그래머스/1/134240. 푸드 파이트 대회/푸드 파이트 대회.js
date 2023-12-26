38903c8b35c177026c417169598878219e3e2c88
CHCHAENG <oco6029@naver.com>
2023-12-26 14:37:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,9 @@
 function solution(food) {
-  let answer = "";
-  
-  for (let i = 1; i < food.length; i++) {
-    answer += String(i).repeat(Math.floor(food[i] / 2));
-  }
-
-  return answer + "0" + [...answer].reverse().join("");
+    let answer = "";
+    
+    for (let i = 1; i < food.length; i++) {
+        answer += i.toString().repeat(parseInt(food[i] / 2));
+    }
+    
+    return answer + "0" + answer.split("").reverse().join("");
 }
