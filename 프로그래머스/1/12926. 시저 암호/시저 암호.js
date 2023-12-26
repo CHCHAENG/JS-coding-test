b17ce00170a4eb61285026366d538c3055d49b91
CHCHAENG <oco6029@naver.com>
2023-12-26 14:08:19 +0900

    [level 1] Title: 시저 암호, Time: 1.67 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,34 +1,26 @@
 function solution(s, n) {
-    let answer = '';
-    for(let i = 0; i < s.length; i++) {
-        let code; 
-      // s[i]가 " "인 경우 answer에 " "문자열을 더해주었다.
-        if(s[i] === " ") {
+    let answer = "";
+    
+    [...s].forEach((v) => {
+        const val = v.charCodeAt();
+
+        if (val >= 65 && val <= 90) {
+            let next = val + n;
+            
+            if (next > 90) {
+                next -= 26;   
+            } 
+            answer += String.fromCodePoint(next);
+        } else if (val >= 97 && val <= 122) {
+            let next = val + n;
+            
+            if (next > 122) {
+                next -= 26;   
+            } 
+            answer += String.fromCodePoint(next);
+        } else {
             answer += " ";
         }
-      // code라는 변수에 해당 아스키코드값을 넣어준다.
-        code = s.charCodeAt(i);
-      // 아스키코드 번호가 이에 해당될 때 
-        if(code >= 65 && code <= 90) {
-            if(code + n > 90) {
-              // String.fromCharCode를 통해 해당하는 아스키코드 값의 문자를 answer에 추가해 준다.
-              // 이 때 대문자의 경우는 90을 넘으면 대문자가 아닌 다른 문자이므로
-              // -26을 해주어서 Z 다음에 A부터 시작하도록 하였다.
-                answer += String.fromCharCode(code + n - 26);
-            } else {
-                answer += String.fromCharCode(code+n);
-            }
-        }
-      // 위와 마찬가지로 ! 이것은 소문자 아스키코드 값 해당하면
-        if(code >= 97 && code <= 122) {
-          // 아스키 코드의 값이 122가 넘어가면 소문자가 아닌 다른 문자이기 때문에
-            if(code + n > 122) {
-              // -26을 해주어서 z 다음에 a가 되도록 해준다.
-                answer += String.fromCharCode(code + n - 26);
-            } else {
-                answer += String.fromCharCode(code+n);
-            }
-        }
-    }
+    })
     return answer;
 }
