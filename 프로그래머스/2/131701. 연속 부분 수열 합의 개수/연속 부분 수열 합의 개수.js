2fe11a565017eb69c093a590dd3a8441ab38c519
CHCHAENG <oco6029@naver.com>
2024-01-02 18:43:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 62.20 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,13 +2,24 @@ function solution(elements) {
     let answer = new Set();
     const length = elements.length;
     
-    for (let i = 0; i < length; i++) {
+    for (let i = 1; i <= length; i++) {
+        let rear = 0;
         let sum = 0;
         
-        for (let j = i; j < length + i; j++) {
-            sum += elements[j % length];
+        for (let j = 0; j < length; j++) {
+            if (j === 0) {
+                while (rear < i) {
+                    sum += elements[rear++];
+                }
+            }
+            else {
+                sum -= elements[j - 1];
+                if (rear === length) rear = 0;
+                sum += elements[rear++];
+            }
             answer.add(sum);
         }
     }
+    
     return answer.size;
 }
