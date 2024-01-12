dcff8b2fb9e74341dcadc9f20d938d2aae0cd358
CHCHAENG <oco6029@naver.com>
2024-01-12 17:43:40 +0900

    [level Lv.1] Title: 과일 장수, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/Lv.1/135808. 과일 장수/과일 장수.js
@@ -1,18 +1,11 @@
 function solution(k, m, score) {
     let answer = 0;
-    let list = [];
     
-    // 이익이 발생하지 않는 경우
-    if (score.length < m) return 0;
+    score.sort((a, b) => b - a);
     
-    // 오름차순으로 정렬
-    score.sort((a, b) => a - b);
-    // 남는 사과 제외
-    score.splice(0, score.length % m);
-    
-    for (let i = 0; i < score.length; i += m) {
-        answer += score[i] * m;
+    for (let i = 0; i < score.length; i = i + m) {
+        const min = score[i + m - 1];
+        if (min) answer += min * m;
     }
-    
-    return answer;
+  return answer;
 }
