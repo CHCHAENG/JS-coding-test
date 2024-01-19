8f8584c413ef3cdbb100867e2fb85b0561e56073
CHCHAENG <oco6029@naver.com>
2024-01-19 17:41:44 +0900

    [level 2] Title: 기능개발, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.14 ms
+메모리: 33.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 21:52:45
+2024년 1월 5일 17:41:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,24 @@
 function solution(progresses, speeds) {
     let answer = [];
+    let i = 0;
+    let cnt = 0;
     
-    while (progresses.length > 0) {
-        
-        for (let i in speeds) {
-            if (progresses[i] < 100)
-                progresses[i] += speeds[i];
+    while (progresses.length) {
+        i = Math.ceil((100 - progresses[0]) / speeds[0]);
+
+        for (let j = 1; j < progresses.length; j++) {
+            progresses[j] += i * speeds[j];
         }
         
+        cnt = 1;
         
-        let cnt = 0;
-        while (progresses[0] >= 100) {
-            progresses.shift();
-            speeds.shift();
-            cnt++;
+        for (let j = 1; j < progresses.length; j++) {
+            if (progresses[j] >= 100) cnt++;
+            else break;
         }
-        
-        if (cnt > 0) answer.push(cnt);
+        answer.push(cnt);
+        progresses.splice(0, cnt);
+        speeds.splice(0, cnt);
     }
     
     return answer;
