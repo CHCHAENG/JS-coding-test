f23735211035919caf5971c8771efdffd4a4f6ef
CHCHAENG <oco6029@naver.com>
2024-01-20 15:43:19 +0900

    [level 2] Title: 타겟 넘버, Time: 1.20 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -2,16 +2,16 @@ function solution(numbers, target) {
     let answer = 0;
     dfs(0, 0);
 
-    function dfs(length, sum) {
+    function dfs (length, val) {
         if (length === numbers.length) {
-            if (target === sum)
+            if (val === target) 
                 answer++;
             return;
         }
         
-        dfs(length + 1, sum + numbers[length]);
-        dfs(length + 1, sum - numbers[length]);
+        dfs(length + 1, val + numbers[length]);
+        dfs(length + 1, val - numbers[length]);
     }
-        
+    
     return answer;
 }
