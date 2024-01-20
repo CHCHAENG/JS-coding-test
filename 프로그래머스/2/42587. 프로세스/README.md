b5733913436c6aa66af4f75d056e2c9469f178c8
CHCHAENG <oco6029@naver.com>
2024-01-20 15:24:26 +0900

    [level 2] Title: 프로세스, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.40 ms
+메모리: 33.6 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 16:30:56
+2024년 1월 6일 15:24:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,34 +1,18 @@
 function solution(priorities, location) {
-    let arr = Array.from(Array(priorities.length), (_, index) => index + 1);
     let answer = [];
-
-    while (arr.length > 0) {
-        let val = arr.shift(); 
-        let pVal = priorities.shift();
+    let arr = Array.from({length : priorities.length}, (_, i) => i);
+    
+    while (priorities.length) {
+        const max = Math.max(...priorities);
+        const val = priorities.shift();
+        const index = arr.shift();
         
-        if (priorities.some((i) => pVal < i)) {
-            arr.push(val);
-            priorities.push(pVal);
+        if (val >= max) {
+            answer.push(index);
         } else {
-            answer.push(val - 1);
+            priorities.push(val);
+            arr.push(index);
         }
-//         let flag = 0;
-    
-//         for (let i = 1; i < arr.length; i++) {
-//             if (priorities[0] < priorities[i]) {
-//                 flag = 1;
-//                 break;
-//             }
-//         }
-        
-//         if (flag === 1) {
-//             arr.push(val);
-//             priorities.push(priorities.shift());
-//         } else  {
-//             answer.push(val - 1);
-//             priorities.shift();
-//         }
     }
-    
     return answer.indexOf(location) + 1;
 }
