04ac9863e97b5401c8c65de53cae8381b22be93a
CHCHAENG <oco6029@naver.com>
2024-01-22 22:25:08 +0900

    [level 2] Title: 모음 사전, Time: 2.17 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.06 ms
+메모리: 36.3 MB, 시간: 2.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 22:53:16
+2024년 1월 1일 22:25:4
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,18 @@
 function solution(word) {
-  const obj = {
-    A: 0,
-    E: 1,
-    I: 2,
-    O: 3,
-    U: 4,
-  };
-  const plus = [781, 156, 31, 6, 1];
-  return word
-    .split("")
-    .reduce((acc, ch, idx) => acc + obj[ch] * plus[idx] + 1, 0);
+    const arr = ['A', 'E', 'I', 'O', 'U'];
+    const list = [];
+    
+    const dfs = (length, str) => {
+        if (length <= 5) {
+            list.push(str);
+            
+            for (let i = 0; i < 5; i++) {
+                dfs(length + 1, str + arr[i]);
+            }
+        }
+    }
+    
+    dfs(0, '');
+    
+    return list.indexOf(word);
 }
