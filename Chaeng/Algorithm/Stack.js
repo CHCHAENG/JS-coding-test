ab93aed72eed1ea24c758057df497ca006a72d83
CHCHAENG <oco6029@naver.com>
2024-02-07 23:23:00 +0900

    Stack 구현

---
+++ Chaeng/Algorithm/Stack.js
@@ -0,0 +1,41 @@
+class Stack {
+    constructor() {
+        this.storage = new Object();
+        this.size = 0;
+    }
+    
+    push(element) {
+        this.size++;
+        this.storage[this.size] = element;
+    }
+    
+    pop() {
+        let removed = this.storage[this.size];
+        delete this.storage[this.size];
+        this.size--;
+        
+        return removed;
+    }
+    
+    size() {
+        return this.size;
+    }
+    
+    top() {
+        return this.storage[this.size];
+    }
+}
+
+let stack = new Stack();
+
+stack.push(3);
+stack.push(5);
+console.log(stack.top());
+stack.push(7);
+console.log(stack.top());
+stack.push(2);
+console.log(stack.top());
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
