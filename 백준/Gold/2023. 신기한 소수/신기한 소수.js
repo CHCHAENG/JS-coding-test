8b70c6be5da39c052289c2382a6de2c292133610
CHCHAENG <oco6029@naver.com>
2024-02-14 15:06:16 +0900

    골드5 백트랙킹으로 다시 풀음

--- 백준/Gold/2023. 신기한 소수/신기한 소수.js
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.js
@@ -4,38 +4,41 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let primeNum = [2, 3, 5, 7];
+let nextNum = [1, 3, 7, 9];
+
 // 소수 판별 함수
 const isPrime = (num) => {
     if (num === 1) return false;
-    if (num === 2) return true;
+    if (num <= 3) return true;
     
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
-
     return true;
 }
 
 // 신기한 소수 판별 함수
-const makeNum = (num) => {
-    while (num > 0) {
-        if (!isPrime(num)) return;
-        num = parseInt(num / 10);
+const makeNum = (N, num) => {
+    if (num.toString().length === N) {
+        console.log(num);
+        return;
     }
     
-    return true;
+    for (let i = 0; i < 4; i++) {
+        const newNum = num * 10 + nextNum[i];
+        
+        if (isPrime(newNum)) 
+            makeNum(N, newNum);
+    }
 }
 
 rl.on('line', function(input) {
     let N = +input;
     
-    let start = 1 * Math.pow(10, N - 1);
-    let end = '9'.repeat(N);
-
-    for (let i = start; i <= +end; i++) {
-        if (makeNum(i)) console.log(i);
+    for (let i = 0; i < 4; i++) {
+        makeNum(N, primeNum[i]);
     }
-    
 }).on('close', () =>{
     process.exit();
 });
