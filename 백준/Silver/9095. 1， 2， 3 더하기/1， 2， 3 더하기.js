680f1a890cb0935742ad7a1ef6a667c536729024
CHCHAENG <oco6029@naver.com>
2024-02-27 10:09:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 144 ms, Memory: 9488 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -5,29 +5,22 @@ const rl = readline.createInterface({
 });
 
 function solution(n) {
+    // n은 11보다 작음
     let arr = new Array(12).fill(0);
+    
+    // 1,2,3 으로 수를 표현하는 것이므로 고정값으로 설정
     arr[1] = 1;
     arr[2] = 2;
     arr[3] = 4;
     
     if (n <= 3) console.log(arr[n]);
     else {
+        // n이 4이상일 경우
         for (let i = 0; i <= n - 4; i++) {
-            fillArr(4 + i, arr);
+            arr[4 + i] = arr[3 + i] + arr[2 + i] + arr[1 + i];
         }
-        
         console.log(arr[n]);
     }
-}
-
-function fillArr(n, arr) {
-    let sum = 0;
-    
-    for (let i = 1; i <= 3; i++) {
-        sum += arr[n - i];
-    }
-
-    arr[n] = sum;
 }
 
 let input;
