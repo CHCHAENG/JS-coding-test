6b7d7bd405a3f291bce0f28dd9ec1805cd18f21e
CHCHAENG <oco6029@naver.com>
2024-03-13 15:14:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 204 ms, Memory: 9784 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+function solution(N, list) {
+    let dp = new Array(N).fill(0);
+    
+    for (let i = 0; i < N; i++) {
+        let count = 0;
+        for (let j = 0; j < i; j++) {
+            if (dp[j] > count && list[i] > list[j]) {
+                count = dp[j];
+            }
+        }
+        dp[i] = ++count;
+    }
+    
+    console.log(Math.max(...dp));
+}
+
+let N = 0;
+let list = [];
+
+rl.on('line', function(line) {
+    if (N !== 0) {
+        list = line.split(" ").map(Number);
+    } else {
+        N = +line;
+    }
+}).on('close', () => {
+    solution(N, list);
+    process.exit();
+});
